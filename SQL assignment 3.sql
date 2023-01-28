aff54c0ef996e03f45e27d23be463db9f06b380f
omerfidanci <33489679+omerfidanci@users.noreply.github.com>
2023-01-28 04:13:07 +0100

    Add files via upload

---
+++ SQL assignment 3.sql
@@ -0,0 +1,29 @@
+﻿--- SQL ASSIGNMENT 3
+
+/*
+Discount Effects
+
+Generate a report including product IDs and discount effects on whether the increase in the 
+discount rate positively impacts the number of orders for the products.
+
+In this assignment, you are expected to generate a solution using SQL with a logical approach. */
+
+WITH disc AS
+	(SELECT	product_id, 
+            COALESCE((COUNT(order_id) - LAG(COUNT(order_id)) OVER (PARTITION BY product_id ORDER BY discount)), 0) as order_count_change
+    FROM sale.order_item oi
+    GROUP BY product_id, discount
+    HAVING COUNT(product_id) > 1 ),
+	
+	disc_eff AS
+	(SELECT disc.product_id, SUM(disc.order_count_change) as total_diff,
+		CASE 
+			WHEN SUM(disc.order_count_change) = 0 THEN 'neutral'
+			WHEN SUM(disc.order_count_change) > 0 THEN 'positive'
+			WHEN SUM(disc.order_count_change) < 0 THEN 'negative'
+		END AS discount_effect
+	FROM disc
+	GROUP BY disc.product_id)
+
+SELECT product_id, discount_effect
+FROM disc_eff
